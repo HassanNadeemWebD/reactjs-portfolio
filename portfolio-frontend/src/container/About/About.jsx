23a097202fa89f1de4f6100fd72967000715383a
HassanNadeemWebD <hn178139@gmail.com>
2022-04-03 15:37:04 +0500

    commit

--- portfolio-frontend/src/container/About/About.jsx
+++ portfolio-frontend/src/container/About/About.jsx
@@ -2,17 +2,18 @@ import React, { useState, useEffect } from 'react'
 import './About.scss';
 
 import { motion } from 'framer-motion'
+import {images} from "../../constants"
 
 const abouts = [{
-  title: 'Web Development', description: 'I am good at web development.', imgUrl: ''
+  title: 'Web Design', description: 'I am good at Web Design.', imgUrl: images.about01
 },
 {
-  title: 'Web Development', description: 'I am good at web development.', imgUrl: ''
+  title: 'UX/UI', description: 'I am good at UX/UI.', imgUrl: images.about02
 },
 {
-  title: 'Web Development', description: 'I am good at web development.', imgUrl: ''
+  title: 'Web Development', description: 'I am good at web development.', imgUrl: images.about03
 },
-{ title: 'Web Development', description: 'I am good at web development.', imgUrl: '' }
+{ title: 'Web Engineer', description: 'I am good at Web Engineer.', imgUrl: images.about04 }
 ];
 
 
@@ -35,7 +36,7 @@ const About = () => {
             key={about.title + index}>
               <img src={about.imgUrl} alt={about.title} />
               <h2 className='bold-text' style={{marginTop: 20 }} >{about.title}</h2>
-              <p className='p-text' style={{marginTop: 20 }} >{about.title}</p>
+              <p className='p-text' style={{marginTop: 20 }} >{about.description}</p>
 
           </motion.div>
         ))}
