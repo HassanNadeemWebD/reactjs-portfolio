2f8439bc48dba884b19f6796ba36cc12552806bf
HassanNadeemWebD <hn178139@gmail.com>
2022-04-03 14:56:04 +0500

    commit

---
+++ portfolio-frontend/src/container/Skills/Skills.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Skills.scss';
+
+
+
+const Skills = () => {
+  return (
+    <div>skills</div>
+  )
+}
+
+export default Skills
---
+++ portfolio-frontend/src/container/Testimonials/Testimonials.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Testimonials.scss';
+
+
+
+
+
+
+const Testimonials = () => {
+  return (
+    <div>testimonials</div>
+  )
+}
+
+export default Testimonials
